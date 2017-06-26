c9b0465f392d8631ff084019159f762feb19f42f
James Thomas <jthomas.uk@gmail.com>
2017-06-26 16:43:50 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,8 +12,8 @@ provider "ibmcloud" {
   bluemix_api_key = "${var.bxapikey}"
 }
 
-data "ibmcloud_cf_service_instance" "twilio" {
-  name  = "twilio"
+data "ibmcloud_cf_service_instance" "cloudant" {
+  name  = "cloudant"
 }
 
 variable bxapikey {
@@ -23,6 +23,6 @@ variable bxapikey {
 ##############################################################################
 # Outputs
 ##############################################################################
-output "twilio_key" {
-  value = "${ibmcloud_cf_service_instance.twilio.credentials}"
+output "cloudant_key" {
+  value = "${ibmcloud_cf_service_instance.cloudant.credentials}"
 }
