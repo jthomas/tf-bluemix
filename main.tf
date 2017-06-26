e6221f7b9bf73ba496340ccf5be6f04ae35db418
James Thomas <jthomas.uk@gmail.com>
2017-06-26 16:29:10 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,48 +10,20 @@ terraform {
 # See the README for details on ways to supply these values
 provider "ibmcloud" {
   bluemix_api_key = "${var.bxapikey}"
-  softlayer_username = "${var.slusername}"
-  softlayer_api_key = "${var.slapikey}"
 }
 
-##############################################################################
-# IBM SSH Key: For connecting to VMs
-##############################################################################
-resource "ibmcloud_infra_ssh_key" "ssh_key" {
-  label = "${var.key_label}"
-  notes = "${var.key_note}"
-  # Public key, so this is completely safe
-  public_key = "${var.public_key}"
+data "ibmcloud_cf_service_key" "serviceKeydata" {
+  name  = "twilio"
+  service_instance_name = "twilio"
 }
 
-##############################################################################
-# Variables
-##############################################################################
 variable bxapikey {
   description = "Your Bluemix API Key."
-}
-variable slusername {
-  description = "Your Softlayer username."
-}
-variable slapikey {
-  description = "Your Softlayer API Key."
-}
-variable datacenter {
-  description = "The datacenter to create resources in."
-}
-variable public_key {
-  description = "Your public SSH key material."
-}
-variable key_label {
-  description = "A label for the SSH key that gets created."
-}
-variable key_note {
-  description = "A note for the SSH key that gets created."
 }
 
 ##############################################################################
 # Outputs
 ##############################################################################
-output "ssh_key_id" {
-  value = "${ibmcloud_infra_ssh_key.ssh_key.id}"
+output "twilio_key" {
+  value = "${ibmcloud_cf_service_key.serviceKeydata.twilio}"
 }
